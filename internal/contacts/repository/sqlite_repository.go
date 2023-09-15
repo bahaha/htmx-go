a0fd7d20a52c9485cb0a2f1ebfda6c7bb8fd582d
Cheng Che <clementcheng56@gmail.com>
2023-09-16 00:37:56 +0800

    feat: view a contact by its id

--- internal/contacts/repository/sqlite_repository.go
+++ internal/contacts/repository/sqlite_repository.go
@@ -40,3 +40,16 @@ func (r *SqliteContactRepository) List(keyword string) ([]contacts.Contact, erro
 
 	return res, nil
 }
+
+func (r *SqliteContactRepository) Find(id int) (*contacts.Contact, error) {
+  row := r.DB.QueryRow("SELECT id, first_name, last_name, phone, email FROM contacts WHERE id = ?", id)
+
+  var contact contacts.Contact
+  err := row.Scan(&contact.ID, &contact.FirstName, &contact.LastName, &contact.Phone, &contact.Email)
+  if err != nil {
+    return nil, err
+  }
+
+  return &contact, nil
+}
+
