c5659c0178a9e7988c22562331d3c83117107d23
bahaha <clementcheng56@gmail.com>
2023-09-19 15:26:29 +0800

    feat: delete contact from the edit view (#4)

--- internal/contacts/repository/repository.go
+++ internal/contacts/repository/repository.go
@@ -3,8 +3,8 @@ package repository
 import "htmx-go/internal/contacts"
 
 type ContactRepository interface {
-  List(keyword string) ([]contacts.Contact, error)
-  Find(id int) (*contacts.Contact, error)
-  Update(contact *contacts.Contact) error
+	List(keyword string) ([]contacts.Contact, error)
+	Find(id int) (*contacts.Contact, error)
+	Update(contact *contacts.Contact) error
+	Delete(id int) error
 }
-
