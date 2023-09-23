0d910aee9088c8198aaadd220a929e26bd8d5536
Cheng Che <clementcheng56@gmail.com>
2023-09-24 02:22:52 +0800

    feat: drop forms with htmx

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"path/filepath"
 
@@ -16,14 +17,18 @@ func main() {
 	r := gin.Default()
 	r.HTMLRender = loadTemplates("web/templates")
 	handlers := initHandlers()
+	v2_handlers := initV2Handlers()
 	r.GET("/", func(c *gin.Context) {
-		c.Redirect(http.StatusFound, "/contacts")
+		c.Redirect(http.StatusFound, "/v2")
 	})
 	r.GET("/contacts", handlers.ListContacts)
 	r.GET("/contacts/:id/edit", handlers.ContactViewToEdit)
 	r.POST("/contacts/:id/edit", handlers.EditContact)
 	r.GET("/contacts/:id", handlers.FindContact)
 	r.POST("/contacts/:id/delete", handlers.DeleteContact)
+
+	r.GET("/v2", v2_handlers.Index)
+	r.GET("v2/contacts", v2_handlers.DisplayContacts)
 
 	r.Run(":55688")
 }
@@ -38,8 +43,26 @@ func initHandlers() *handler.ContactHandler {
 	return &handler.ContactHandler{Repo: repo}
 }
 
+func initV2Handlers() *handler.ContactV2Handler {
+	db, err := database.DataSource()
+	if err != nil {
+		panic(err)
+	}
+
+	repo := &repository.SqliteContactRepository{DB: db}
+	return &handler.ContactV2Handler{Repo: repo}
+}
+
 func loadTemplates(templateDir string) multitemplate.Renderer {
 	renderer := multitemplate.NewRenderer()
+
+	templates, err := filepath.Glob(templateDir + "/*.html")
+
+	for _, template := range templates {
+		pageName := filepath.Base(template)
+		fmt.Println(pageName)
+		renderer.AddFromFiles(pageName, template)
+	}
 
 	layouts, err := filepath.Glob(templateDir + "/layout/*.html")
 	if err != nil {
